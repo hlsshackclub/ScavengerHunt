b1f0fa53daf3de1575332e20cdfa52cb98ca23bc
Liam West <awesomelax7@gmail.com>
2025-05-02 14:20:18 -0400

    terminal almost done, only perms left

--- terminal.js
+++ terminal.js
@@ -1,5 +1,12 @@
 const terminalMessages = [];
-const terminalPrompt = "[root@HLSS";
+const terminalPrompt = "@HLSS";
+
+class User {
+    constructor(name, home) {
+	this.name = name;
+	this.home = home;
+    }
+}
 
 class Directory {
     constructor(name, parent, perms) {
@@ -7,6 +14,10 @@ class Directory {
         this.parent = parent;
         this.perms = perms;
         this.files = [];
+    }
+
+    typeToString() {
+	return "directory";
     }
 }
 
@@ -17,23 +28,42 @@ class File {
         this.perms = perms;
         this.content = "";
     }
+
+    typeToString() {
+	return "file";
+    }
 }
 
 const root = new Directory("", "", 777);
 let currentPath = root;
+let admin = {};
+let user = {}
+let currentUser = {};
+let users = [];
 
-function initFileSystem() {
-    currentPath.files.push(new Directory("test", currentPath, 777));
-    currentPath.files.push(new Directory("test1", currentPath, 777));
-    getDirectoryByName("test", currentPath).files.push(new Directory("testing", getDirectoryByName("test", currentPath), 777));
+function initSystem() {
+    mkdir(["home"]);
+    mkdir(["home/hacker"]);
+    mkdir(["etc"]);
+    mkdir(["bin"]);
+    mkdir(["lib"]);
+    mkdir(["home/root"]);
+    mkdir(["home/root/SuperSecretFolder"]);
+    touch(["home/root/SuperSecretFolder/password.txt"]);
+    touch(["home/root/SuperSecretFolder/fakePassword.txt"]);
+    user = new User("hacker", getByName("/home/hacker", root, Directory));
+    currentUser = user;
+    users.push(user);
+    admin = new User("root", getByName("/home/root", root, Directory));
+    users.push(admin);
 }
 
-initFileSystem();
+initSystem();
 
 function ls(args) {
     let path = currentPath;
     if (args.length !== 0) {
-        const dir = getDirectoryByName(args[0], currentPath);
+        const dir = getByName(args[0], currentPath, Directory);
         if (dir) path = dir;
     }
 
@@ -41,107 +71,91 @@ function ls(args) {
         if (file instanceof Directory) return `[DIR] ${file.name}`;
         else return file.name;
     }).join(" ");
-    printToConsole(output);
+    return(output);
 }
 
 function cd(args) {
     if (args.length === 0) {
         currentPath = root;
-        return;
+        return "Changed directory to /";
     }
 
-    currentPath = getDirectoryByName(args[0], currentPath);
+    currentPath = getByName(args[0], currentPath, Directory);
+    return `Changed directory to ${args[0]}`;
 }
 
 /*TODO: make perms work*/
 function sudo(args) { }
 
-/*TODO: cat files*/
-function cat(args) { }
-
-/*TODO: combine mkdir and touch*/
-function mkdir(args) {
+function cat(args) {
     if (args.length === 0) {
-        printToConsole("mkdir: missing operand");
-        return;
+	return("cat: missing operand");
     }
 
-    const path = getDirectoryByName(args[0], currentPath);
+    const path = getByName(args[0], currentPath, File);
     if (path) {
-        printToConsole(`mkdir: '${args[0]}': Directory exists`);
-        return
+	return(path.content);
+    } else {
+	return(`cat: '${args[0]}': No such file`);
     }
+}
 
-    let dir = currentPath
-    if (args[0].includes("/")) {
-        let p = args[0].split(0, args[0].lastIndexOf("/"));
-        dir = getDirectoryByName(p, currentDirectory);
-    }
-
-    const newDirectory = new Directory(args[0], dir, 777);
-    dir.files.push(newDirectory);
-    printToConsole(`Created directory ${args[0]}`);
+function mkdir(args) {
+    return create(args, Directory, "mkdir");
 }
 
 function touch(args) {
+    return create(args, File, "touch");
+}
+
+function create(args, type, command) {
     if (args.length === 0) {
-        printToConsole("touch: missing operand");
-        return;
+        return(`${command}: missing operand`);
     }
 
-    const path = getFileByName(args[0], currentPath);
+    const path = getByName(args[0], currentPath, type);
     if (path) {
-        printToConsole(`touch: '${args[0]}': File exists`);
-        return;
+        return(`${command}: '${args[0]}' exists`);
     }
 
     let dir = currentPath
+    let name = args[0];
 
     if (args[0].includes("/")) {
         /*remove file name (text after the last /) to get path*/
-        let p = args[0].split(0, args[0].lastIndexOf("/"));
-        dir = getDirectoryByName(p, currentDirectory);
+        let p = args[0].slice(0, args[0].lastIndexOf("/"));
+	name = args[0].slice(args[0].lastIndexOf("/") + 1);
+        dir = getByName(p, currentPath, Directory);
     }
 
-    const newFile = new File(args[0], dir, 777);
-    dir.files.push(newFile);
-    printToConsole(`Created file ${args[0]}`);
+    const n = new type(name, dir, 777);
+    dir.files.push(n);
+    return(`Created ${args[0]}`);
 }
 
-/*TODO: Combine rmdir and rm*/
 function rmdir(args) {
-    if (args.length === 0) {
-        printToConsole("rm: missing operand");
-        return;
-    }
-
-    const path = getDirectoryByName(args[0], currentPath);
-    if (path) {
-        const index = currentPath.files.indexOf(path);
-        if (index > -1) {
-            currentPath.files.splice(index, 1);
-            printToConsole(`Removed ${path.name}`);
-        }
-    } else {
-        printToConsole(`rm: cannot remove '${args[0]}': No such directory`);
-    }
+    return r(args, Directory, "rmdir");
 }
 
 function rm(args) {
+    return r(args, File, "rm");
+}
+
+function r(args, type, command) {
     if (args.length === 0) {
-        printToConsole("rm: missing operand");
-        return;
+        return(`${command}: missing operand`);
     }
 
-    const path = getFileByName(args[0], currentPath);
+    const path = getByName(args[0], currentPath, type);
     if (path) {
         const index = currentPath.files.indexOf(path);
-        if (index > -1) {
-            currentPath.files.splice(index, 1);
-            printToConsole(`Removed ${path.name}`);
-        }
+	if (path instanceof Directory && path.files.length > 0) {
+	    return(`${command}: '${args[0]}': Directory not empty`);
+	}
+        path.parent.files.splice(index, 1);
+        return(`Removed ${path.name}`);
     } else {
-        printToConsole(`rm: cannot remove '${args[0]}': No such file`);
+        return(`${command}: cannot remove '${args[0]}': Does not exist or is not a ${type.prototype.typeToString()}`);
     }
 }
 
@@ -157,50 +171,41 @@ function getPathString(p) {
     return "/" + segments.filter(seg => seg !== "").join("/");
 }
 
-/*TODO: combine gFBN and gDBN, also add root indexing (/) and home indexing (~/)*/
-function getFileByName(name, p) {
+function getByName(name, p, type) {
     let path = p;
+    if (name[0] === "/") {
+	path = root;
+	name = name.slice(1);
+    }
     const parts = name.split("/");
     for (let part of parts) {
-        if (part === "..") {
-            if (path.parent != "") path = path.parent;
-        } else {
-            const next = path.files.find(file => file instanceof File && file.name === part);
-            if (next) path = next;
-            else {
-                return null;
-            }
-        }
+	if (part === "..") {
+	    if (path.parent != root) path = path.parent;
+	} else {
+	    const next = path.files.find(file => file.name === part);
+	    if (next) path = next;
+	    else {
+		return null;
+	    }
+	}
     }
-    return path;
-}
-
-function getDirectoryByName(name, p) {
-    let path = p;
-    const parts = name.split("/");
-    for (let part of parts) {
-        if (part === "..") {
-            if (path.parent != "") path = path.parent;
-        } else {
-            const next = path.files.find(file => file instanceof Directory && file.name === part);
-            if (next) path = next;
-            else {
-                return null;
-            }
-        }
-    }
-    return path;
+    return path instanceof type ? path : null;
 }
 
 function enter(x) {
     let val = x.children[1].value;
     x.children[1].value = "";
-    terminalMessages.push(terminalPrompt + getPathString(currentPath) + "]$ " + val);
+    let tPrompt = "[" + currentUser.name + terminalPrompt + getPathString(currentPath) + "]$ "
+    terminalMessages.push(tPrompt + val);
     executeCommand(val);
-    x.children[0].innerText = terminalPrompt + getPathString(currentPath) + "]$ ";
+    x.children[0].innerText = tPrompt;
     let messagesString = terminalMessages[0];
     terminalMessages.slice(1).forEach(message => {
-        messagesString += ("<br>" + htmlEscape(message));
+	if (message === "") {
+	    messagesString += "<br> ";
+	} else {
+            messagesString += ("<br>" + htmlEscape(message));
+	}
     });
     document.getElementById("terminal").innerHTML = messagesString;
 }
@@ -209,14 +214,35 @@ function printToConsole(s) {
     terminalMessages.push(s);
 }
 
-/*TODO: echo piping*/
 function echo(args) {
+    if  (args.includes(">>")) {
+	const index = args.indexOf(">>");
+	const fileName = args[index + 1];
+	const file = getByName(fileName, currentPath, File);
+	if (file) {
+	    file.content += args.slice(0, index).join(" ");
+	    return(`Wrote to ${fileName}`);
+	} else {
+	    return(`echo: '${fileName}': No such file`);
+	}
+    }
+    if (args.includes(">")) {
+	const index = args.indexOf(">");
+	const fileName = args[index + 1];
+	const file = getByName(fileName, currentPath, File);
+	if (file) {
+	    file.content = args.slice(0, index).join(" ");
+	    return(`Wrote to ${fileName}`);
+	} else {
+	    return(`echo: '${fileName}': No such file`);
+	}
+    }
     printToConsole(args.join(" "));
 }
 
 function help(args) {
-    printToConsole("help - displays this message");
     printToConsole("echo - display a line of text");
+    return("help - displays this message");
 }
 
 function parseArgs(input) {
@@ -251,7 +277,7 @@ function executeCommand(input) {
     const func = functions[funcName];
 
     if (typeof func === "function") {
-        func(funcArgs);
+        printToConsole(func(funcArgs));
     } else {
         printToConsole(`Command "${funcName}" not found. Did you mean help?`);
     }
