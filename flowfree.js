0842eadbbcb2446617620b825e1d31f6cf80b10a
Empika1 <dravidts1@gmail.com>
2025-05-08 16:51:36 -0400

    flow free works!!!!!!!!!!

--- flowfree.js
+++ flowfree.js
@@ -1,4 +1,4 @@
-function makeFlowFreeGame(width, height, starts, onWin) {
+function makeFlowFreeGame(width, height, starts) {
     let paths = []
     for (const _ of starts) {
         paths.push([])
@@ -6,15 +6,22 @@ function makeFlowFreeGame(width, height, starts, onWin) {
 
     let held = false
     let oldPaths = []
+    clearOldPaths()
     let currentPathI = undefined
 
-    function populatePaths(from, to, excludes) {
-        to.length = 0
+    function clearOldPaths() {
+        oldPaths.length = 0
+        for (const _ of starts) {
+            oldPaths.push([])
+        }
+    }
+
+    function populatePaths(from, to, exclude) {
         for (const [i, path] of from.entries()) {
-            if (excludes !== undefined && excludes.includes(i)) {
+            if (exclude === i) {
                 continue
             }
-            to.push(path.slice())
+            to[i] = path.slice()
         }
     }
 
@@ -24,6 +31,15 @@ function makeFlowFreeGame(width, height, starts, onWin) {
                 if (v2Eq(pPos, pos)) {
                     return i
                 }
+            }
+        }
+        return undefined
+    }
+
+    function getIInPath(pos, pathI) {
+        for (const [i, pPos] of paths[pathI].entries()) {
+            if (v2Eq(pPos, pos)) {
+                return i
             }
         }
         return undefined
@@ -40,37 +56,54 @@ function makeFlowFreeGame(width, height, starts, onWin) {
         return undefined
     }
 
-    function trimPath(pos) {
+    function isCompletedPath(pathI) {
+        return pathI !== undefined && paths[pathI].length > 1
+            && getStart(paths[pathI].slice(-1)[0]) !== undefined
+    }
+
+    function trimPaths(pos, exclude) {
         for (const [i, path] of paths.entries()) {
+            if (i === exclude) {
+                return
+            }
             for (const [j, pPos] of path.entries()) {
                 if (v2Eq(pPos, pos)) {
                     path.length = j
-                    console.log(path.length)
-                    return
                 }
             }
         }
     }
 
-    function makeBoardState() {
-        let board = []
-        for (let x = 0; x < width; x++) {
-            board.push([])
-            for (let y = 0; y < height; y++) {
-                board[x].push(undefined)
+    function checkWin() {
+        for(const start of starts) {
+            nextSPos: for(const sPos of start) {
+                for(const path of paths) {
+                    for(const pPos of path) {
+                        if(v2Eq(sPos, pPos)) {
+                            continue nextSPos
+                        }
+                    }
+                }
+                return false
             }
         }
-        for (const [i, path] of paths.entries()) {
-            for (const pPos of path) {
-                board[pPos[0]][pPos[1]] = i
-            }
-        }
-        return board
+        return true
     }
 
     function click(pos) {
-        if (getStart(pos) === undefined && getPath(pos) === undefined) {
+        const start = getStart(pos)
+        const pathI = getPath(pos)
+        if (start === undefined && pathI === undefined) {
             return;
+        }
+        if (start !== undefined && paths[start].length !== 0
+            && v2MDist(paths[start].slice(-1)[0], pos) !== 1) {
+            paths[start].length = 0
+        } else if (isCompletedPath(pathI)) {
+            const iInPath = getIInPath(pos, pathI);
+            if(paths[pathI].length - 1 - iInPath < iInPath) {
+                paths[pathI].reverse()
+            }
         }
         held = true;
         populatePaths(paths, oldPaths)
@@ -78,32 +111,53 @@ function makeFlowFreeGame(width, height, starts, onWin) {
 
     function release() {
         held = false;
-        oldPaths.length = 0
+        clearOldPaths()
         currentPathI = undefined
     }
 
     function hoverOver(pos) {
-        (() => {
+        inner: {
             if (!held) {
-                return
+                break inner
             }
             if (pos[0] < 0 || pos[1] < 0 || pos[0] >= width || pos[1] >= height) {
-                return
+                break inner
             }
-            if (currentPathI !== undefined 
+
+            const cursorStartI = getStart(pos);
+            
+            let cursorPathI = cursorStartI
+            if (cursorPathI === undefined) {
+                cursorPathI = getPath(pos);
+            }
+
+            if (currentPathI !== undefined
+                && currentPathI !== cursorPathI
                 && paths[currentPathI].length > 0
                 && v2MDist(paths[currentPathI].slice(-1)[0], pos) !== 1) {
-                return
+                break inner
             }
 
-            if(currentPathI === undefined) {
-                currentPathI = getStart(pos);
+            if(isCompletedPath(currentPathI) && currentPathI !== cursorPathI) {
+                break inner
             }
 
-            trimPath(pos)
+            if (currentPathI === undefined) {
+                currentPathI = cursorPathI
+            }
+
+            if (cursorStartI !== undefined && cursorStartI !== currentPathI) {
+                break inner
+            }
+
+            populatePaths(oldPaths, paths, currentPathI)
+            trimPaths(pos)
+            for (const pPos of paths[currentPathI]) {
+                trimPaths(pPos, currentPathI)
+            }
             paths[currentPathI].push(pos)
-        })()
-        return paths;
+        }
+        return {paths: paths, win: checkWin()};
     }
 
     return {
@@ -128,7 +182,8 @@ function setupFlowFree(width, height, starts, nodeID, winFunc) {
     const ftss = container.querySelectorAll(".flex-table-holder");
     const flowFree = ftss[ftss.length - 1]; //add to the last one found bcs that's the one you just created
 
-    let game = makeFlowFreeGame(width, height, starts, undefined)
+    let game = makeFlowFreeGame(width, height, starts)
+    let alreadyWon = false
 
     let cells = []
     function render(paths) {
@@ -188,15 +243,23 @@ function setupFlowFree(width, height, starts, nodeID, winFunc) {
 
             td.addEventListener("mousedown", event => {
                 game.click(cell)
-                const paths = game.hoverOver(cell)
-                render(paths)
+                const state = game.hoverOver(cell)
+                if(state.win && !alreadyWon) {
+                    alreadyWon = true
+                    winFunc()
+                }
+                render(state.paths)
             })
             td.addEventListener("mouseup", event => {
                 game.release(cell)
             })
             td.addEventListener("mouseover", event => {
-                const paths = game.hoverOver(cell)
-                render(paths)
+                const state = game.hoverOver(cell)
+                if(state.win && !alreadyWon) {
+                    alreadyWon = true
+                    winFunc()
+                }
+                render(state.paths)
             })
 
             tr.appendChild(td);
@@ -209,219 +272,6 @@ function setupFlowFree(width, height, starts, nodeID, winFunc) {
     table.appendChild(tbody);
     flowFree.appendChild(table);
 }
-
-//TODO: make this not break with non-square boards because i mixed up row and col somewhere
-// function setupFlowFree(width, height, starts, nodeID, winFunc) {
-//     const container = document.getElementById(nodeID);
-//     container.innerHTML += `
-//     <p>${"#".repeat(width * 3 + 2)}</p>
-//     <div class="flex-table-holder-holder">
-//         <p>${"#<br>".repeat(height * 3)}
-//         </p>
-//         <div class="flex-table-holder flowFree"></div>
-//         <p>${"#<br>".repeat(height * 3)}
-//         </p>
-//     </div>
-//     <p>${"#".repeat(width * 3 + 2)}</p>`
-//     const ftss = container.querySelectorAll(".flex-table-holder");
-//     const flowFree = ftss[ftss.length - 1]; //add to the last one found bcs that's the one you just created
-
-//     let currentPath = undefined
-
-//     let cells = []
-
-//     const paths = []
-//     for (let i = 0; i < starts.length; i++) {
-//         paths.push([]);
-//     }
-
-//     function getCurrentPath(cell) {
-//         for (const [i, startPair] of starts.entries()) {
-//             if ((startPair[0][0] == cell[0] && startPair[0][1] == cell[1])
-//                 || (startPair[1][0] == cell[0] && startPair[1][1] == cell[1])) {
-//                 return i
-//             }
-//         }
-
-//         for (const [i, path] of paths.entries()) {
-//             for (let pathCell of path) {
-//                 if (pathCell[0] == cell[0] && pathCell[1] == cell[1]) {
-//                     return i
-//                 }
-//             }
-//         }
-//         return undefined
-//     }
-
-//     function trimPath(cell) {
-//         //account for the fact you can break a path and start from the other side
-//         let pathFound = undefined
-//         let startUsed = undefined
-//         outer: for (const [i, startPair] of starts.entries()) {
-//             for (const [j, start] of startPair.entries()) {
-//                 if (v2Eq(start, cell)) {
-//                     pathFound = i
-//                     startUsed = j
-//                     break outer
-//                 }
-//             }
-//         }
-//         if (pathFound !== undefined
-//             && paths[pathFound].length != 0
-//             && !v2Eq(paths[pathFound][0], starts[pathFound][startUsed])
-//             && v2MDist(paths[pathFound].slice(-1)[0], cell) > 1) {
-//             paths[pathFound].length = 0
-//         } else {
-//             //trim the path normally
-//             for (const path of paths) {
-//                 for (const [j, pathCell] of path.entries()) {
-//                     if (pathCell[0] == cell[0] && pathCell[1] == cell[1]) {
-//                         path.length = j
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-//     function render() {
-//         for (let row = 0; row < height; row++) {
-//             for (let col = 0; col < width; col++) {
-//                 for (let i = 0; i < starts.length; i++) {
-//                     cells[col][row].notEmojiDiv.classList.remove(`c${i}`);
-//                     cells[col][row].notEmojiDiv.innerText = ""
-//                 }
-//             }
-//         }
-
-//         for (const [i, path] of paths.entries()) {
-//             for (const [j, pathCell] of path.entries()) {
-//                 cells[pathCell[1]][pathCell[0]].notEmojiDiv.classList.add(`c${i}`);
-//                 let text = undefined
-//                 if (path.length == 1) {
-//                     text = '';
-//                 } else {
-//                     if (j == 0) {
-//                         text = connectionsBackwards[JSON.stringify([v2Sub(path[j + 1], pathCell)])];
-//                     } else if (j < path.length - 1) {
-//                         text = connectionsBackwards[JSON.stringify([v2Sub(path[j + 1], pathCell), v2Sub(path[j - 1], pathCell)])];
-//                     } else {
-//                         text = connectionsBackwards[JSON.stringify([v2Sub(path[j - 1], pathCell)])];
-//                     }
-//                 }
-//                 cells[pathCell[1]][pathCell[0]].notEmojiDiv.innerText = text
-//             }
-//         }
-//     }
-
-//     function isEnd(cell) {
-//         const possibleStart1 = starts[currentPath][0];
-//         const possibleStart2 = starts[currentPath][1];
-//         const thisStart = paths[currentPath][0];
-//         return !v2Eq(cell, thisStart) && (v2Eq(cell, possibleStart1) || v2Eq(cell, possibleStart2));
-//     }
-
-//     function won() {
-//         for (const startPair of starts) {
-//             checkStart: for (const start of startPair) {
-//                 for (const path of paths) {
-//                     if (path.length == 0) {
-//                         continue;
-//                     }
-//                     if (v2Eq(start, path[0]) || v2Eq(start, path.slice(-1)[0])) {
-//                         continue checkStart;
-//                     }
-//                 }
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-//     let wonAlready = false
-
-//     const table = document.createElement("table");
-//     const tbody = document.createElement("tbody");
-//     for (let row = 0; row < height; row++) {
-//         const tr = document.createElement("tr");
-//         cells.push([]);
-//         for (let col = 0; col < width; col++) {
-//             const cell = [col, row];
-//             const td = document.createElement("td");
-//             td.classList.add(`cb${(row + col) % 2}`);
-
-//             const div = document.createElement("div");
-//             div.classList.add("not-emoji");
-//             td.appendChild(div);
-
-//             let emojiDiv = undefined
-//             for (const [i, startPair] of starts.entries()) {
-//                 if (v2Eq(startPair[0], cell) || v2Eq(startPair[1], cell)) {
-//                     emojiDiv = document.createElement("div");
-//                     emojiDiv.classList.add("emoji");
-//                     emojiDiv.classList.add(`c${i}`);
-//                     emojiDiv.innerText = v2Eq(startPair[0], cell) ? "◆" : "◉"
-//                     td.appendChild(emojiDiv);
-//                     break;
-//                 }
-//             }
-
-//             td.addEventListener("mouseover", event => {
-//                 if (currentPath === undefined) {
-//                     return;
-//                 }
-//                 if (paths[currentPath].length >= 1 && isEnd(paths[currentPath].slice(-1)[0]) && getCurrentPath(cell) != currentPath) { //past the end
-//                     return;
-//                 }
-//                 let isDiffStart = false;
-//                 for (const [i, startPair] of starts.entries()) {
-//                     if (currentPath == i) {
-//                         continue;
-//                     }
-//                     if ((startPair[0][0] == cell[0] && startPair[0][1] == cell[1])
-//                         || (startPair[1][0] == cell[0] && startPair[1][1] == cell[1])) {
-//                         isDiffStart = true
-//                     }
-//                 }
-//                 const isRightPos = paths[currentPath].length == 0
-//                     || Math.abs(paths[currentPath].slice(-1)[0][0] - cell[0]) + Math.abs(paths[currentPath].slice(-1)[0][1] - cell[1]) == 1
-//                 const isOnSamePath = getCurrentPath(cell) == currentPath
-//                 if (isDiffStart || (!isRightPos && !isOnSamePath)) {
-//                     return
-//                 }
-//                 trimPath(cell)
-//                 paths[currentPath].push(cell);
-//                 render();
-//                 if (!wonAlready && won()) {
-//                     wonAlready = true
-//                     winFunc()
-//                 }
-//             })
-//             td.addEventListener("mousedown", event => {
-//                 currentPath = getCurrentPath(cell);
-//                 if (currentPath === undefined) {
-//                     return;
-//                 }
-//                 trimPath(cell)
-//                 paths[currentPath].push(cell);
-//                 render();
-//                 if (!wonAlready && won()) {
-//                     wonAlready = true
-//                     winFunc()
-//                 }
-//             })
-//             td.addEventListener("mouseup", event => {
-//                 currentPath = undefined;
-//             })
-
-//             tr.appendChild(td);
-//             cells[row].push({ td: td, emojiDiv: emojiDiv, notEmojiDiv: div });
-//         }
-
-//         tbody.appendChild(tr);
-//     }
-
-//     table.appendChild(tbody);
-//     flowFree.appendChild(table);
-// }
 
 function setupAllFlowFrees() {
     setupFlowFree(7, 7,
