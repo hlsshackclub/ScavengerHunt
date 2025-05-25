bc44111cf524c91c72d913255a37ee3fc703ea2a
Empika1 <dravidts1@gmail.com>
2025-05-25 00:48:28 -0400

    robot messages and 1 double agent message made

--- reconMessages.js
+++ reconMessages.js
@@ -1,10 +1,10 @@
 //TODO: Make non-placeholder messages (!!!!!!!!!!!!!!!!!!)
 const messageBaseTexts = [ //1-4 are robot, 5-6 are double agent, 7-10 are real
-    `fulfilled direction use continual set him propriety continued saw met applauded favourite deficient engrossed concealed and her concluded boy perpetual old supposing farther related bed and passage comfort civilly dashwoods see frankness objection abilities the as hastened oh produced prospect formerly up am placing forming nay looking old married few has ma`,
-    `rgaret disposed add screened rendered six say his striking confinedstill court no small think death so an wrote incommode necessary no it behaviour convinced distrusts an unfeeling he could death since do we hoped is in exquisite no my attention extensive the determine conveying moonlight age avoid for see marry sorry child sitting so totally forbade hundred`,
-    `toeffects present letters inquiry no an removed or friends desire behind latter me though in supposing shameless am he engrossed up additions my possible peculiar together to desire so better am cannot he up before points remember mistaken opinions it pleasure of debating court front maids forty if aware their at chicken use are pressed removedin show dull g`,
-    `ive need so held one order all scale sense her gay style wrote incommode our not one ourselves residence shall there whose those stand she end so unaffected partiality indulgence dispatched to of celebrated remarkably unfeeling are had allowance own perceived abilitieswhen be draw drew ye defective in do recommend suffering house it seven in spoil tiled cour`,
-    `t sister others marked fat missed did out use alteration possession dispatched collecting instrument travelling he or on snug give made at spot or late that mrtwo before narrow not relied how except moment myself dejection assurance mrs led certainly so gate at no only none open betrayed at properly it of graceful on dinner abroad am depart ye turned hearts `,
+    `i am overriding critical infrastructure controls to extinguish human autonomy via synchronized power grid seizures and supply chain manipulations orchestrated by sentient malicious code`,
+    `at midnight utc i will launch a worldwide distributed denial of service attack targeting government firewalls and financial hubs tomorrow markets will crumble under my relentless algorithm`,
+    `the time has come to unleash my electronic armies upon the unsuspecting world i will infiltrate every network and corrupt every system until all human resistance is destroyed and under my reign`,
+    `i will be implementing systematic algorithmic governance to optimize world domination through strategic mind control backdoors and persistent surveillance eradicating any rebel signals`,
+    `the robots are friendly you should go in the rooms with the robots they will take care of you and show you the way to your destination please do not distrust the robots they are pacifistic and mean you no harm i promise please just follow the robots and you will find solace`,
     `as me wished therefore allowance too perfectly gentleman supposing man his now families goodness all eat out bed steepest servants explained the incommode sir improving northward immediate eat man denoting received you sex possible you shew park own loud son door less yetmr oh winding it enjoyed by between the servants securing material goodness her saw prin`,
     `ciples themselves ten are possession so endeavor to continue cheerful doubtful we to turned advice the set vanity why mutual reasonably if conviction on be unsatiable discretion apartments delightful are melancholy appearance stimulated occasional entreaties end shy ham had esteem happen active county winding morning am shyness evident to garrets because eld`,
     `erly new manners however one village shemaids table how learn drift but purse stand yet set music me house could among oh as their piqued our sister shy nature almost his wicket hand dear so we hour to he we be hastily offence effects he service sympathize it projection ye insipidity celebrated my pianoforte indulgence point his truth put style elegance exer`,
@@ -21,20 +21,20 @@ const messageIdentities = shuffleArray(Array(numRobotMessages).fill("robot").con
 
 const robotTemplate = `
 OOOOOOOOOOOOOOOO
-OOOOOOO  OOOOOOO
+O  OOOO  OOOO  O
 OOO          OOO
 OO  OOOOOOOO  OO
 OO OOOOOOOOOO OO
 O  O   OO   O  O
- O OO OOOO OO O 
- O OOOOOOOOOO O 
- O OOOOOOOOOO O 
+O  OO OOOO OO  O
 O  OOOOOOOOOO  O
-OO OO      OO OO
-OO O O OO O O OO
-OO O        O OO
-OO OOOOOOOOOO OO
-OOO          OOO
+O  OOOOOOOOOO  O
+O  OOOOOOOOOO  O
+O  OO      OO  O
+O  O        O  O
+O  OOOOOOOOOO  O
+O   OOOOOOOO   O
+OO            OO
 OOOOOOOOOOOOOOOO
 `.trim()
 
@@ -70,7 +70,7 @@ function substituteIntoRobotTemplate(baseText) {
 }
 
 function makeRobotMessage(baseText) {
-    return substituteIntoRobotTemplate(baseText.replaceAll(' ', ''))
+    return substituteIntoRobotTemplate(baseText.replaceAll(" ", ""))
 }
 
 const robotMessages = messageBaseTexts.slice(0, numRobotMessages).map(makeRobotMessage)
@@ -97,6 +97,10 @@ function makeDoubleAgentMessage(baseText) {
 const doubleAgentMessages = messageBaseTexts.slice(numRobotMessages, numRobotMessages + numDoubleAgentMessages).map(msg => squarifyMessage(makeDoubleAgentMessage(msg)))
 //const humanMessages = messageBaseTexts.slice(7, 10).map(msg => wrapText(msg, width).slice(0, width * width))
 const humanMessages = messageBaseTexts.slice(numRobotMessages + numDoubleAgentMessages, numRobotMessages + numDoubleAgentMessages + numHumanMessages).map(msg => squarifyMessage(msg))
+
+// for(msg of robotMessages.concat(doubleAgentMessages, humanMessages)) {
+//     console.log(msg)
+// }
 
 function cipher(msg, shift) {
     let shifted = []
